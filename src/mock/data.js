0032050d08ddfc48510567c69d0f4dc7f6da6d5b
Eren A. Akyol <ahmederen123@gmail.com>
2020-12-17 21:17:17 +0300

    update

--- src/mock/data.js
+++ src/mock/data.js
@@ -2,56 +2,41 @@ import { nanoid } from 'nanoid';
 
 // HEAD DATA
 export const headData = {
-  title: '', // e.g: 'Name | Developer'
-  lang: '', // e.g: en, es, fr, jp
-  description: '', // e.g: Welcome to my website
+  title: 'Eren Ahmed Akyol', // e.g: 'Name | Developer'
+  lang: 'tr', // e.g: en, es, fr, jp
+  description: 'Siteme hoşgeldiniz', // e.g: Welcome to my website
 };
 
 // HERO DATA
 export const heroData = {
-  title: '',
-  name: '',
-  subtitle: '',
-  cta: '',
+  title: "I'm",
+  name: 'Eren Ahmed Akyol',
+  subtitle: 'Welcome to my world',
+  cta: 'Continue',
 };
 
 // ABOUT DATA
 export const aboutData = {
   img: 'profile.jpg',
-  paragraphOne: '',
-  paragraphTwo: '',
-  paragraphThree: '',
-  resume: 'https://www.resumemaker.online/es.php', // if no resume, the button will not show up
+  paragraphOne:
+    "My name is Eren Ahmed, I'm 16 years old and I'm a software developer for 4 years. I am the founder of RedMC Network for 1 year. It's a game server project for Minecraft related.",
+  paragraphTwo:
+    'I love learning programming languages and making software. Sometimes for money but usually for hobby. My favorite programming language is Go. Already I know PHP, JavaScript, Python and little Ruby and C#',
+  paragraphThree: 'I working remotely now and I study for university.',
+  resume: 'https://www.resumemaker.online/en.php', // if no resume, the button will not show up
 };
 
 // PROJECTS DATA
 export const projectsData = [
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
-  },
-  {
-    id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
-  },
-  {
-    id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    img: 'redmc.png',
+    title: 'RedMC Network',
+    info: 'My game server RedMC Network website.',
+    info2:
+      "It's has shop, vote, blog for players. And may pages. Written with NextJS and Go for backend.",
+    url: 'https://redmc.me',
+    repo: '', // if no repo, the button will not show up
   },
 ];
 
@@ -59,7 +44,7 @@ export const projectsData = [
 export const contactData = {
   cta: '',
   btn: '',
-  email: '',
+  email: 'ahmederen123@gmail.com',
 };
 
 // FOOTER DATA
@@ -68,27 +53,17 @@ export const footerData = {
     {
       id: nanoid(),
       name: 'twitter',
-      url: '',
-    },
-    {
-      id: nanoid(),
-      name: 'codepen',
-      url: '',
-    },
-    {
-      id: nanoid(),
-      name: 'linkedin',
-      url: '',
+      url: 'https://twitter.com/alphaeren59',
     },
     {
       id: nanoid(),
       name: 'github',
-      url: '',
+      url: 'https://github.com/eren5960',
     },
   ],
 };
 
 // Github start/fork buttons
 export const githubButtons = {
-  isEnabled: true, // set to false to disable the GitHub stars/fork buttons
+  isEnabled: false, // set to false to disable the GitHub stars/fork buttons
 };
